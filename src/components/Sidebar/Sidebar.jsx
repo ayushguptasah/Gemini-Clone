e9f113a360bea722abfb177d907e09d841cf82ac
ayushguptasah <ayushguptasah99@gmail.com>
2024-06-22 12:35:44 +0530

    Add sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,36 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
+
   return (
     <div className="sidebar">
       <div className="top">
-        <img className="menu" src={assets.menu_icon} alt="" />
+        <img onClick={()=>setExtended(prev=>!prev)} className="menu" src={assets.menu_icon} alt="" />
         <div className="new-chat">
           <img src={assets.plus_icon} alt="" />
-          <p>New Chat</p>
+          {extended ? <p>New Chat</p> : null}
         </div>
-        <div className="recent">
-          <p className="recent-tittle">Recent</p>
-          <div className="recent-entry">
-            <img src={assets.message_icon} alt="" />
-            <p>What is react ...</p>
+        {extended ? (
+          <div className="recent">
+            <p className="recent-tittle">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="" />
+              <p>What is react ...</p>
+            </div>
           </div>
-        </div>
+        ) : null}
       </div>
       <div className="bottom">
         <div className="bottom-item recent-entry">
           <img src={assets.question_icon} alt="" />
-          <p>Help</p>
+          {extended ? <p>Help</p> : null}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.history_icon} alt="" />
-          <p>Activity</p>
+          {extended ? <p>Activity</p> : null}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.setting_icon} alt="" />
-          <p>Settings</p>
+          {extended ? <p>Settings</p> : null}
         </div>
       </div>
     </div>
