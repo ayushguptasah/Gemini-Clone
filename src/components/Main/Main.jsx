9149aac8da5026310a54da3fb95bcd25ce689617
ayushguptasah <ayushguptasah99@gmail.com>
2024-06-23 18:53:42 +0530

    Add input field

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -34,6 +34,18 @@ const Main = () => {
             <img src={assets.code_icon} alt="" />
           </div>
         </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a prompt here" />
+            <img src={assets.gallery_icon} alt="" />
+            <img src={assets.mic_icon} alt="" />
+            <img src={assets.send_icon} alt="" />
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses. Your privacy and Gemini Apps
+          </p>
+        </div>
       </div>
     </div>
   );
