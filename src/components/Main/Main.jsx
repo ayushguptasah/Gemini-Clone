a2177415feb231200bdd52d51da76480ea875907
ayushguptasah <ayushguptasah99@gmail.com>
2024-06-26 00:26:15 +0530

    Add onclick

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Main.css";
 import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
 
 const Main = () => {
+
+  const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context)
+
   return (
     <div className="main">
       <div className="nav">
@@ -36,10 +40,10 @@ const Main = () => {
         </div>
         <div className="main-bottom">
           <div className="search-box">
-            <input type="text" placeholder="Enter a prompt here" />
+            <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder="Enter a prompt here" />
             <img src={assets.gallery_icon} alt="" />
             <img src={assets.mic_icon} alt="" />
-            <img src={assets.send_icon} alt="" />
+            <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
           </div>
           <p className="bottom-info">
             Gemini may display inaccurate info, including about people, so
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,26 +1,36 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import runChat from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading,setLoading] = useState(false);
+  const [resultData,setResultData] = useState("");
 
+  const onSent = async (prompt) => {
+    await runChat(input);
+  };
 
-    const onSent = async (prompt) => {
-        await runChat(prompt)
-    }
+  const contextValue = {
+    prevPrompts,
+    setPrevPrompts,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput
+  };
 
-    onSent("What is react")
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
 
-    const contextValue = {
-
-    }
-
-    return (
-        <Context.Provider value={contextValue}>
-            {props.children}
-        </Context.Provider>
-    )
-}
-
-export default ContextProvider
+export default ContextProvider;
